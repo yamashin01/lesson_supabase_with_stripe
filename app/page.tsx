b9936dcaa59bbb42ba4a2fa1e02e7a1d910ed79f
yamada <shinya.yamada@benext-corp.com>
2024-08-23 11:39:25 +0900

    ♻️ Create getAllLessons function

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,15 @@ import { createServerComponentClient } from "@/node_modules/@supabase/auth-helpe
 import { cookies } from "@/node_modules/next/headers";
 import Link from "@/node_modules/next/link";
 
-export default async function Home() {
-  const supabase = createServerComponentClient({cookies});
+const supabase = createServerComponentClient({cookies});
+const getAllLessons = async() => {
   const { data: lessons } = await supabase.from("lesson").select("*");
+  return lessons;
+
+}
+
+export default async function Home() {
+  const lessons = await getAllLessons();
 
   return (
     <main className="w-full max-w-3xl mx-auto my-16 px-2">

0b0a4249957566f71b2cd58cdc23852106861056
yamada <shinya.yamada@benext-corp.com>
2024-08-23 11:02:56 +0900

    💄 Add card style to lesson

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { createServerComponentClient } from "@/node_modules/@supabase/auth-helpers-nextjs/dist/index";
 import { cookies } from "@/node_modules/next/headers";
 import Link from "@/node_modules/next/link";
@@ -8,10 +9,20 @@ export default async function Home() {
 
   return (
     <main className="w-full max-w-3xl mx-auto my-16 px-2">
-      {lessons?.map((lesson) => (
-        <Link href={`/${lesson.id}`} key={lesson.id}>{lesson.title}</Link>
-      ))}
-
+      <div className="flex flex-col gap-3">
+        {lessons?.map((lesson) => (
+          <Link href={`/${lesson.id}`} key={lesson.id}>
+            <Card>
+              <CardHeader>
+                <CardTitle>{lesson.title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>{lesson.description}</p>
+              </CardContent>
+            </Card>
+          </Link>
+        ))}
+      </div>
     </main>
   );
 }

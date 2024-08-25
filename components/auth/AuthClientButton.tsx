9700cab7149a066a1d1972179532b1978c550924
yamada <shinya.yamada@benext-corp.com>
2024-08-25 17:17:53 +0900

    ✨ Add handleSignout function

--- components/auth/AuthClientButton.tsx
+++ components/auth/AuthClientButton.tsx
@@ -4,14 +4,34 @@ import {
   createClientComponentClient,
   Session,
 } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 import { Button } from "../ui/button";
 
 const AuthClientButton = ({ session }: { session: Session | null }) => {
+  const router = useRouter();
   const supabase = createClientComponentClient();
   const handleSignIn = async () => {
-    await supabase.auth.signInWithOAuth({ provider: "github" });
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: `${location.origin}/auth/callback`,
+      },
+    });
   };
-  return <Button onClick={handleSignIn}>サインイン</Button>;
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+  return (
+    <>
+      {session ? (
+        <Button onClick={handleSignOut}>サインアウト</Button>
+      ) : (
+        <Button onClick={handleSignIn}>サインイン</Button>
+      )}
+    </>
+  );
 };
 
 export default AuthClientButton;

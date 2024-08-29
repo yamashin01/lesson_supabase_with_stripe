167c5c4b983af07db1763bead7729c0f6015c093
yamada <shinya.yamada@benext-corp.com>
2024-08-30 08:17:49 +0900

    ✨ Add Youtube to lesson detail page

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -5,6 +5,7 @@ import {
 import React from "react";
 import { cookies } from "@/node_modules/next/headers";
 import { Database } from "@/lib/database.types";
+import { YouTubeEmbed } from "@next/third-parties/google";
 
 const getDetailLesson = async (
   id: number,
@@ -18,14 +19,29 @@ const getDetailLesson = async (
   return lessons;
 };
 
+const getPremiumContent = async (
+  id: number,
+  supabase: SupabaseClient<Database>
+) => {
+  const { data: video } = await supabase
+    .from("premium_content")
+    .select("video_url")
+    .eq("id", id)
+    .single();
+  return video;
+};
+
 const LessonDetailPage = async ({ params }: { params: { id: number } }) => {
   const supabase = createServerComponentClient<Database>({ cookies });
   const lesson = await getDetailLesson(params.id, supabase);
+  const video = await getPremiumContent(params.id, supabase);
+  const videoId = video?.video_url.match(/v=([^&]+)/)[1];
 
   return (
     <div className="w-full max-w-3xl mx-auto py-16 px-8">
       <h1 className="text-3xl mb-6">{lesson?.title}</h1>
       <p className="mb-8">{lesson?.description}</p>
+      <YouTubeEmbed videoid={videoId} />
     </div>
   );
 };

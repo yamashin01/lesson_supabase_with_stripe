4c52965fe97c00981724ef0d1e1621e579bf40a7
yamada <shinya.yamada@benext-corp.com>
2024-09-02 08:27:29 +0900

    ✨ Add pricingPage

---
+++ app/pricing/page.tsx
@@ -0,0 +1,58 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import initStripe from "stripe";
+
+const getAllPlans = async () => {
+  const stripe = new initStripe(process.env.STRIPE_SECRET_KEY!);
+  const { data: plans } = await stripe.plans.list();
+
+  const planInfoList = await Promise.all(
+    plans.map(async (plan) => {
+      const product = await stripe.products.retrieve(plan.product as string);
+
+      return {
+        id: plan.id,
+        name: product.name,
+        price: plan.amount_decimal,
+        interval: plan.interval,
+        currency: plan.currency,
+      };
+    })
+  );
+
+  const sortedPlanList = planInfoList.sort((a, b) => a.price! - b.price!);
+
+  return sortedPlanList;
+};
+
+const PricingPage = async () => {
+  const planInfoList = await getAllPlans();
+
+  return (
+    <div className="w-full flex max-w-3xl mx-auto py-16 justify-around">
+      {planInfoList.map((plan) => (
+        <Card className="shadow-md" key={plan.id}>
+          <CardHeader>
+            <CardTitle>{plan.name}</CardTitle>
+            <CardDescription>{plan.interval}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            {plan.price}/{plan.interval}
+          </CardContent>
+          <CardFooter>
+            <Button>サブスクリプション契約する</Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default PricingPage;

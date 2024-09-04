9a3884eb3f70454cee30a78084a62f6f57befdcc
yamada <shinya.yamada@benext-corp.com>
2024-09-05 07:45:50 +0900

    ✨ Add stripe webhook

---
+++ app/api/stripe-hooks/route.ts
@@ -0,0 +1,47 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { NextRequest, NextResponse } from "next/server";
+import initStripe from "stripe";
+import { cookies } from "next/headers";
+import { Database } from "@/lib/database.types";
+
+export async function POST(req: NextRequest) {
+  const supabase = createRouteHandlerClient<Database>({ cookies });
+  const stripe = new initStripe(process.env.STRIPE_SECRET_KEY!);
+  const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+  const signature = req.headers.get("stripe-signature");
+
+  const reqBuffer = Buffer.from(await req.arrayBuffer());
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      reqBuffer,
+      signature!,
+      endpointSecret!
+    );
+
+    switch (event.type) {
+      case "customer.subscription.created":
+        const customerSubscriptionCreated = event.data.object;
+        await supabase
+          .from("profile")
+          .update({
+            is_subscribed: true,
+            interval: customerSubscriptionCreated.items.data[0].plan.interval,
+          })
+          .eq("stripe_customer", event.data.object.customer);
+        break;
+      case "customer.subscription.updated":
+        break;
+      case "customer.subscription.deleted":
+        break;
+      default:
+        break;
+    }
+
+    return NextResponse.json({ received: true });
+  } catch (err: any) {
+    return NextResponse.json(`webhook Error: ${err.message}`, { status: 401 });
+  }
+}

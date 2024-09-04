06ef357370850539229782c144bef1e361b7db67
yamada <shinya.yamada@benext-corp.com>
2024-09-05 08:33:49 +0900

    💄 Add dashboard to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,25 @@
+import { Database } from "@/lib/database.types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import Link from "next/link";
 import React from "react";
 import AuthServerButton from "./auth/AuthServerButton";
 import { Button } from "./ui/button";
+import { cookies } from "@/node_modules/next/headers";
 
-const Header = () => {
+const Header = async () => {
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const { data: user } = await supabase.auth.getSession();
+
   return (
     <div className="flex border-b py-4 px-6 border-gray-200">
       <Link href={"/"}>
         <Button variant="outline">ホーム</Button>
       </Link>
+      {user.session && (
+        <Link href={"/dashboard"} className="ml-4">
+          <Button variant="outline">ダッシュボード</Button>
+        </Link>
+      )}
       <Link href={"/pricing"} className="ml-4">
         <Button variant="outline">価格</Button>
       </Link>
